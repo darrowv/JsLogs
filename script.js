8c238e56abc3f1e71fae798d552072762a1590fa
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-01-17 19:54:11 +0300

    basic functionality

--- script.js
+++ script.js
@@ -22,11 +22,18 @@ runBtn.addEventListener("click", () => {
   // это здесь, чтобы список логов каждый раз обновлялся
   outputList.innerHTML = "";
 
-  eval(inputArea.value);
+  try {
+    eval(inputArea.value);
+  } catch (error) {
+    console.log(new Error("Error: " + error.message));
+  }
 
   console.logs.forEach((log) => {
     let li = document.createElement("li");
-    if (typeof log === "object") {
+    if (log instanceof Error) {
+      li.textContent = log.message;
+      li.style.color = "red";
+    } else if (typeof log === "object") {
       li.textContent = JSON.stringify(log);
     } else if (typeof log === "string") {
       li.textContent = `"${log}"`;
@@ -37,68 +44,16 @@ runBtn.addEventListener("click", () => {
   });
 });
 
+// hotkeys
+document.addEventListener("keydown", (e) => {
+  if(e.ctrlKey && e.key === "Enter") {
+    runBtn.click();
+  } else if (e.ctrlKey && e.key === "\\") {
+    clearBtn.click();
+  }
+});
+
 clearBtn.addEventListener("click", () => {
   console.clear();
   outputList.innerHTML = "";
 });
-
-
-// script for resizing divs
-
-const resizer = document.getElementById('resize');
-const leftSide = resizer.previousElementSibling;
-const rightSide = resizer.nextElementSibling;
-
-// The current position of mouse
-let x = 0;
-let y = 0;
-
-// Width of left side
-let leftWidth = 0;
-
-// Handle the mousedown event
-// that's triggered when user drags the resizer
-const mouseDownHandler = function (e) {
-    // Get the current mouse position
-    x = e.clientX;
-    y = e.clientY;
-    leftWidth = leftSide.getBoundingClientRect().width;
-
-    // Attach the listeners to `document`
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-};
-
-// Attach the handler
-resizer.addEventListener('mousedown', mouseDownHandler);
-
-const mouseMoveHandler = function (e) {
-  // How far the mouse has been moved
-  const dx = e.clientX - x;
-  const dy = e.clientY - y;
-
-  const newLeftWidth = ((leftWidth + dx) * 100) / resizer.parentNode.getBoundingClientRect().width;
-  leftSide.style.width = `${newLeftWidth}%`;
-
-  leftSide.style.userSelect = 'none';
-  leftSide.style.pointerEvents = 'none';
-
-  rightSide.style.userSelect = 'none';
-  rightSide.style.pointerEvents = 'none';
-};
-
-const mouseUpHandler = function () {
-  resizer.style.removeProperty('cursor');
-  // document.body.style.removeProperty('cursor');
-
-  leftSide.style.removeProperty('user-select');
-  leftSide.style.removeProperty('pointer-events');
-
-  rightSide.style.removeProperty('user-select');
-  rightSide.style.removeProperty('pointer-events');
-
-  // Remove the handlers of `mousemove` and `mouseup`
-  document.removeEventListener('mousemove', mouseMoveHandler);
-  document.removeEventListener('mouseup', mouseUpHandler);
-};
-
