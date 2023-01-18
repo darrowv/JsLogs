484ae54cefda0283c29b3e819b9c3aa5b27e8144
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-01-18 14:11:55 +0300

    migrated to codemirror from highlight.js

--- themeToggle.js
+++ themeToggle.js
@@ -1,11 +1,44 @@
-let toggle = document.getElementById("toggle");
-toggle.addEventListener("click", modeSwitch);
+let currentTheme = localStorage.getItem("theme");
+
+let themeToggle = document.getElementById("themeToggle");
+
+let pageTheme = document.body;
 
 let isLight = true;
 
-function modeSwitch() {
-  isLight = !isLight;
-  isLight ? toggle.innerText = "🌞" : toggle.innerText = "🌚";
-  var rootElement = document.body;
-  rootElement.classList.toggle("lightMode");
+if (currentTheme == "light") {
+  pageTheme.classList.add("light-theme");
+  themeToggle.innerText = "🌚"
+} else {
+  themeToggle.innerText="🌞";
 }
+
+function themeMode() {
+  isLight = !isLight;
+  isLight ? themeToggle.innerText = "🌞" : themeToggle.innerText = "🌚";
+  pageTheme.classList.toggle("light-theme");
+  
+  let theme = "dark";
+  if (pageTheme.classList.contains("light-theme")) {
+    theme = "light";
+  }
+  localStorage.setItem("theme", theme);
+}
+
+themeToggle.addEventListener("click", themeMode);
+
+
+// codemirror settings
+
+var editor = CodeMirror.fromTextArea(
+  document.getElementById("inputArea"),
+  {
+    mode: "javascript",
+    theme: currentTheme === "dark" ? "dracula" : "default",
+    tabSize: 2,
+    autoCloseBrackets: true,
+    autofocus: true,
+    matchBrackets: true,
+    highlightSelectionMatches: true
+  }
+);
