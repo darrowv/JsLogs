6334ea8a14e906381cec836370418dd7938dac47
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-01-20 01:40:25 +0300

    added code snippets such as cl+tab writing console.log etc

--- themeToggle.js
+++ themeToggle.js
@@ -1,12 +1,41 @@
 // codemirror settings
 
+var customSnippets = {
+  "cl": 'console.log();',
+  "cc": "console.clear();",
+  "fd": "function name() {\n\n};",
+  "af": "let foo = () => {};",
+  "lorem": `"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ab eveniet ipsa hic quod, ipsam quia quo? Nam consequatur, nostrum ullam consequuntur mollitia ipsa deserunt eaque, ipsam quo sit animi!"`
+}
+
 var editor = CodeMirror.fromTextArea(document.getElementById("inputArea"), {
   mode: "javascript",
   tabSize: 2,
   autoCloseBrackets: true,
   autofocus: true,
   matchBrackets: true,
-  highlightSelectionMatches: true
+  highlightSelectionMatches: true,
+  lineWrapping: true,
+  extraKeys: {
+    "Tab": function(cm) {
+      var cursor = cm.getCursor();
+      var cur = cm.getRange({line: cursor.line, ch: 0}, cursor);
+      var word = cm.findWordAt(cursor);
+      var curWord = cm.getRange(word.anchor, word.head);
+      var snippet = customSnippets[curWord];
+      if (snippet) {
+        cm.replaceRange(snippet, word.anchor, word.head);
+        if(curWord === "cl") {
+          var newCursor = {line: cursor.line, ch: cursor.ch + snippet.indexOf("(") - 1};
+          cm.setCursor(newCursor);
+        }
+      } else {
+        if (cursor.ch === 0 || /^\s+$/.test(cur)) {
+          cm.execCommand("indentMore");
+        }
+      }
+  }
+  },
 });
 
 // theme toggling
