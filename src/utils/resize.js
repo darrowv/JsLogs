c2f3572267405134eab6e42c9640afeaacd7c531
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-01-23 20:34:10 +0300

    updated some variable names

--- src/utils/resize.js
+++ src/utils/resize.js
@@ -1,24 +1,22 @@
 // script for resizing divs
-
-
 let __resizer = document.getElementById("__resize");
 let __leftSide = __resizer.previousElementSibling;
 let __rightSide = __resizer.nextElementSibling;
 
 // The current position of mouse
-let x = 0;
-let y = 0;
+let __x = 0;
+let __y = 0;
 
 // Width of left side
-let leftWidth = 0;
+let __leftWidth = 0;
 
 // Handle the mousedown event
 // that's triggered when user drags the __resizer
 const __mouseDownHandler = function (e) {
   // Get the current mouse position
-  x = e.clientX;
-  y = e.clientY;
-  leftWidth = __leftSide.getBoundingClientRect().width;
+  __x = e.clientX;
+  __y = e.clientY;
+  __leftWidth = __leftSide.getBoundingClientRect().width;
   
   __resizer.style.border = "1px dashed var(--primary-font-color)";
   // Attach the listeners to `document`
@@ -31,10 +29,10 @@ __resizer.addEventListener("mousedown", __mouseDownHandler);
 
 const __mouseMoveHandler = function (e) {
   // How far the mouse has been moved
-  const dx = e.clientX - x;
+  const dx = e.clientX - __x;
 
   const newLeftWidth =
-    ((leftWidth + dx) * 100) / __resizer.parentNode.getBoundingClientRect().width;
+    ((__leftWidth + dx) * 100) / __resizer.parentNode.getBoundingClientRect().width;
   __leftSide.style.width = `${newLeftWidth}%`;
 
 
